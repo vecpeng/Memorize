8e3f24f8bf2105522466b455c99c6db2f822a2d7
vecpeng <841298391cp@gmail.com>
2021-05-05 00:01:59 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 陈鹏 on 2021/5/5.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
